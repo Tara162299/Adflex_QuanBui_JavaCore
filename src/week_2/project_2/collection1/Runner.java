0f6be5b2720af763d91460d95bf4d66213c8c15e
Bui Do Minh Quan <quanbdm@eway.vn>
2021-06-10 05:11:47 +0700

    Proj2 update

--- src/week_2/project_2/collection1/Runner.java
+++ src/week_2/project_2/collection1/Runner.java
@@ -7,10 +7,6 @@ import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
-// nen khai bao interface, dung list thay vi arraylist
-// nguyen tac solid
-// output must be specific
-// always khai bao modifier
 
 public class Runner {
     public static void main(String[] args) {
@@ -21,36 +17,21 @@ public class Runner {
         helperMethods message = new helperMethods(messageInput);
         helperMethods struct = new helperMethods(structInput);
 
-        List<String> toStringStruct = struct.changeToString();
-        // extract all the syntax of the String struct to an arraylist
-
-
         // change message file input to String
         List<String> toStringMessage = message.changeToString();
-        // extract all the date of the String message to an arraylist (checked for future date)
+        // extract all the date of the String message to a list (checked for future date)
         List<String> dateArray = message.originalDateList(toStringMessage);
 
         // choosing numChar base on the syntax that has the most character.
         List<List<String>> SyntaxMessageList = message.getSyntaxinMessage(toStringMessage, 5);
         message.getDiffSyntaxMessage();
 
-        System.out.println(dateArray);
-        System.out.println(SyntaxMessageList);
-        System.out.println();
+        // map each syntax with its number of valid appearances
+        message.countAppearanceEachSyntax();
 
-        Map<List<String>, Integer> test = message.countAppearanceEachSyntax();
-        System.out.println(test);
-
-        System.out.println();
         message.getValidSyntax();
 
-
-
         Map<List<String>, List<Date>> syntaxDateMap = message.mapSyntaxDates();
-        System.out.println(syntaxDateMap);
-
-        System.out.println();
-
 
         for (List<String> syntaxChecking : syntaxDateMap.keySet()) {
             // creating new output file of the valid syntaxs
